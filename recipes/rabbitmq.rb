a0171ebe25b0a9926cac120cd1d042174e7ae894
Greg Thornton <gthornton@cjadvertising.com>
2012-05-14 12:41:40 -0500

    Use rabbitmq cookbook

--- recipes/rabbitmq.rb
+++ recipes/rabbitmq.rb
@@ -20,50 +20,17 @@
 # limitations under the License.
 #
 
-def debian_before_squeeze?
-  platform?("debian") && (node.platform_version.to_f < 5.0 || (node.platform_version.to_f == 5.0 && node.platform_version !~ /.*sid/ ))
+include_recipe "rabbitmq"
+
+# Chef server vhost
+rabbitmq_vhost "/chef" do
+  action :add
 end
 
-if (platform?("ubuntu") && node.platform_version.to_f <= 9.10) || debian_before_squeeze?
-  include_recipe("erlang")
-
-  rabbitmq_dpkg_path = ::File.join(Chef::Config[:file_cache_path], "/", "rabbitmq-server_1.7.2-1_all.deb")
-
-  remote_file(rabbitmq_dpkg_path) do
-    checksum "ea2bbbb41f6d539884498bbdb5c7d3984643127dbdad5e9f7c28ec9df76b1355"
-    source "http://mirror.rabbitmq.com/releases/rabbitmq-server/v1.7.2/rabbitmq-server_1.7.2-1_all.deb"
-  end
-
-  dpkg_package(rabbitmq_dpkg_path) do
-    source rabbitmq_dpkg_path
-    version '1.7.2-1'
-    action :install
-  end
-else
-  package "rabbitmq-server"
-end
-
-service "rabbitmq-server" do
-  if platform?("centos","redhat","fedora")
-    start_command "/sbin/service rabbitmq-server start &> /dev/null"
-    stop_command "/sbin/service rabbitmq-server stop &> /dev/null"
-  end
-  supports [ :restart, :status ]
-  action [ :enable, :start ]
-end
-
-# add a chef vhost to the queue
-execute "rabbitmqctl add_vhost /chef" do
-  not_if "rabbitmqctl list_vhosts| grep /chef"
-end
-
-# create chef user for the queue
-execute "rabbitmqctl add_user chef testing" do
-  not_if "rabbitmqctl list_users |grep chef"
-end
-
-# grant the mapper user the ability to do anything with the /chef vhost
-# the three regex's map to config, write, read permissions respectively
-execute 'rabbitmqctl set_permissions -p /chef chef ".*" ".*" ".*"' do
-  not_if 'rabbitmqctl list_user_permissions chef|grep /chef'
+# Chef server user
+rabbitmq_user "chef" do
+  vhost "/chef"
+  password "testing"
+  permissions "\".*\" \".*\" \".*\""
+  action [:add, :set_permissions]
 end
